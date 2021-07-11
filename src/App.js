5251f839d0eb923a8e4edf2da4ab1516e71ecc03
iradukundairenee <iradukundairenee9@gmail.com>
2021-07-11 13:26:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Content">
+    <div className="Container">
+    <div className="Top">
+      <div className="join">
+    <h3>Join our community</h3>
+      </div>
+      <div className="day">
+        <h4>30 day, hassle free money back guarantee</h4>
+      </div>
+      <div className="gain">
+        gain access to full library  along with expert code reviwes
+        perfect for any developers  who are serious about honing their skills.
+      </div>
+    </div>
+    <div className="Footer">
+         <div className="right">
+           <div>
+             <h3>Monthly Subscription</h3>
+           </div>
+           <div className="per">
+             <span className="Dollars">$29</span><span className="Month">per month</span>
+           </div>
+           <di className="fullaccess">
+             full access less than $1 a day
+           </di>
+           <div className="signup">
+             <button>Sign Up</button>
+           </div>
+         </div>
+          <div className="left">
+           <div>
+            <h3>Why Us</h3> 
+             </div>
+           <div>
+             tutorials by industry expert
+           </div>
+           <div>
+             peer and expert code reviw
+           </div>
+           <div>
+             coding execises
+           </div>
+           <div>
+             access to our github repo
+           </div>
+           <div>
+             community forum
+           </div>
+           <div>
+             Flash Card desk
+           </div>
+           <div>
+             New video every week
+           </div>
+         </div>
+    </div>
+    </div> 
     </div>
   );
 }
